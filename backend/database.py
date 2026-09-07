7085378251b21e1eace0c9de4a56118ad77d4fa8
Harshal Deshmukh <136334910+Harsh-cloudcode@users.noreply.github.com>
2026-09-07 16:53:09 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -8,15 +8,56 @@
 # db = client["resume_analyzer"]
 
 # candidates_collection = db["candidates"]
+# # from pymongo import MongoClient
+# # import os
+
+# # MONGO_URL = os.getenv("MONGO_URL")
+
+# # client = MongoClient(MONGO_URL)
+
+# # db = client["resume_analyzer"]
+
+# # candidates_collection = db["candidates"]
+# from pymongo import MongoClient
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+# MONGO_URL = os.getenv("MONGODB_URI")
+
+# client = MongoClient(MONGO_URL)
+
+# db = client["resume_analyzer"]
+
+# candidates_collection = db["candidates"]
+
+# try:
+#     client.admin.command("ping")
+#     print("MongoDB connected successfully!")
+# except Exception as e:
+#     print("MongoDB connection failed:", e)
+
+
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import certifi 
 
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGODB_URI")
 
-client = MongoClient(MONGO_URL)
+# Pass the certifi certificate authority file to resolve the SSL handshake error
+# client = MongoClient(MONGO_URL, tlsCAFile=certifi.where())
+
+client = MongoClient(
+    MONGO_URL,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=10000,
+    connectTimeoutMS=10000,
+    socketTimeoutMS=20000
+)
 
 db = client["resume_analyzer"]
 
